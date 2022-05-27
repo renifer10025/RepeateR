e687d2cd2a3dc6b7e609aa19016b7da215bac533
Damian Szymecki <minedamixp@gmail.com>
2022-05-27 16:07:18 +0200

    Added basic button events

--- RepeateR/cMain.cpp
+++ RepeateR/cMain.cpp
@@ -1,8 +1,13 @@
 #include "cMain.h"
+#include <conio.h>
+#include <Windows.h>
 
 wxBEGIN_EVENT_TABLE(cMain, wxFrame)
 
 EVT_BUTTON(10001, OnButtonClicked)
+EVT_BUTTON(1, ListenForKeyInput1)
+EVT_BUTTON(2, ListenForKeyInput2)
+EVT_BUTTON(3, ListenForKeyInput3)
 
 wxEND_EVENT_TABLE()
 
@@ -10,16 +15,24 @@ cMain::cMain() : wxFrame(nullptr, wxID_ANY, "RepeateR", wxPoint(30, 30), wxSize(
 {
 	//		Definitions
 
-	macroInput = new wxTextCtrl(this, wxID_ANY, "", wxPoint(10, 10), wxSize(500, 200));
-	btn = new wxButton(this, 10001, "just a button!", wxPoint(520, 10), wxSize(200, 300));
-	loopSequence = new wxCheckBox(this, 1, str, wxPoint(10, 220), wxSize(200, 10), 0, wxDefaultValidator, "loopSequence");
-	//checkboxLoopSequence = new wxStaticText(this, wxID_ANY, "Loop macro sequence?", wxDefaultPosition, wxSize(300, 100), 0, "loopSequenceText");
+	macroCreator1 = new wxButton(this, 1, "Button 1", wxPoint(10, 10), wxSize(100, 20));
+	plus1 = new wxStaticText(this, wxID_ANY, "+", wxPoint(120, 10), wxSize(10, 20), 0, "");
+
+	macroCreator2 = new wxButton(this, 2, "Button 2", wxPoint(140, 10), wxSize(100, 20));
+	plus2 = new wxStaticText(this, wxID_ANY, "+", wxPoint(250, 10), wxSize(10, 20), 0, "");
+
+	macroCreator3 = new wxButton(this, 3, "Button 3", wxPoint(270, 10), wxSize(100, 20));
+
+	
+	macroInput = new wxTextCtrl(this, wxID_ANY, "", wxPoint(10, 50), wxSize(500, 200));
+	btn = new wxButton(this, 10001, "just a button!", wxPoint(570, 10), wxSize(200, 300));
+	loopSequence = new wxCheckBox(this, 1, loopMacroString, wxPoint(10, 270), wxSize(200, 10), 0, wxDefaultValidator, "loopSequence");
 
 
 
 	//		Initializations
 
-	macroInput->SetValue("Input macro sequence, example: S(500) 4(1892) Shift+T(5) Ctrl+3 Alt+F4 Wait(50000)");
+	macroInput->SetValue("Input macro sequence (in ms), example: S(500) 4(1892) Shift+T(5) Ctrl+3 Alt+F4 Wait(50000)");
 	macroInput->SetFocus();
 
 
@@ -34,5 +47,77 @@ cMain::~cMain()
 void cMain::OnButtonClicked(wxCommandEvent& evt)	//			On Button Clicked
 {
 	wxMessageBox(macroInput->GetValue());
+	auxiliary = macroInput->GetValue();
+
+
+
+
+
+	evt.Skip();
+}
+
+void cMain::ListenForKeyInput1(wxCommandEvent& evt)
+{
+	keyInput = true;
+	while (keyInput)
+	{
+		if (GetAsyncKeyState(VK_LCONTROL))
+		{
+			keyInput = false;
+			macroCreator1->SetLabel("Left Control");
+			wxMessageBox("1 Left control pressed!");
+		}
+		if (GetAsyncKeyState(VK_ESCAPE))
+		{
+			keyInput = false;
+			wxMessageBox("Escape pressed on 1");
+		}
+		Sleep(10);
+	}
+
+	evt.Skip();
+}
+
+void cMain::ListenForKeyInput2(wxCommandEvent& evt)
+{
+	keyInput = true;
+	while (keyInput)
+	{
+		if (GetAsyncKeyState(VK_LCONTROL))
+		{
+			keyInput = false;
+			macroCreator2->SetLabel("Left Control");
+			wxMessageBox("2 Left control pressed!");
+		}
+		if (GetAsyncKeyState(VK_ESCAPE))
+		{
+			keyInput = false;
+			wxMessageBox("Escape pressed on 2");
+		}
+		Sleep(10);
+	}
+
+	evt.Skip();
+}
+
+void cMain::ListenForKeyInput3(wxCommandEvent& evt)
+{
+	keyInput = true;
+	while (keyInput)
+	{
+		if (GetAsyncKeyState(VK_LCONTROL))
+		{
+			keyInput = false;
+			macroCreator3->SetLabel("Left Control");
+			wxMessageBox("3 Left control pressed!");
+		}
+		if (GetAsyncKeyState(VK_ESCAPE))
+		{
+			keyInput = false;
+			wxMessageBox("Escape pressed on 3");
+		}
+		Sleep(10);
+	}
+
 	evt.Skip();
 }
--- RepeateR/cMain.h
+++ RepeateR/cMain.h
@@ -8,15 +8,29 @@ public:
 	cMain();
 	~cMain();
 
-	wxString str = "Loop macro sequence?";
+	wxString loopMacroString = "Loop macro sequence?";
+	wxString auxiliary = "";
+	bool keyInput = true;
 
 	wxStaticText* macroText = nullptr;
 	wxTextCtrl* macroInput = nullptr;
+	//wxTextCtrl* macroCreator1 = nullptr;
+	//wxTextCtrl* macroCreator2 = nullptr;
+	//wxTextCtrl* macroCreator3 = nullptr;
+	wxButton* macroCreator1 = nullptr;
+	wxButton* macroCreator2 = nullptr;
+	wxButton* macroCreator3 = nullptr;
 	wxButton* btn = nullptr;		//			Change to more specific name!
 	wxCheckBox* loopSequence = nullptr;
 	wxStaticText* checkboxLoopSequence = nullptr;
 
+	wxStaticText* plus1 = nullptr;
+	wxStaticText* plus2 = nullptr;
+
 	void OnButtonClicked(wxCommandEvent& evt);
+	void ListenForKeyInput1(wxCommandEvent& evt);
+	void ListenForKeyInput2(wxCommandEvent& evt);
+	void ListenForKeyInput3(wxCommandEvent& evt);
 
 	void RemovePlaceholder();
 
